fd3bae2ff5fe84b94aa3cc1d3b192a8a1c98bfc8
vinicius.silva <gasparellovinicius@gmail.com>
2026-05-05 17:06:41 -0300

    feat: criação do projeto infra com dbcontext e configuracoes das entidades

---
+++ SistemaFinanceiro.Infra.Data/Configurations/CategoriaConfiguration.cs
@@ -0,0 +1,17 @@
+﻿namespace SistemaFinanceiro.Infra.Data.Configurations;
+
+public class CategoriaConfiguration : IEntityTypeConfiguration<Categoria>
+{
+    public void Configure(EntityTypeBuilder<Categoria> builder)
+    {
+        builder.ToTable("categoria");
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id).HasColumnName("id");
+        builder.Property(x => x.Descricao).IsRequired().HasMaxLength(200).HasColumnName("descricao");
+        builder.Property(x => x.DirecaoTransacao).IsRequired().HasConversion<int>().HasColumnName("direcaotransacao") ;
+        builder.Property(x => x.Ativo).IsRequired().HasColumnName("ativo");
+
+        builder.HasIndex(x => x.Ativo, "IX_Categoria_Ativo");
+    }
+}
---
+++ SistemaFinanceiro.Infra.Data/DatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿namespace SistemaFinanceiro.Infra.Data;
+
+public class DatabaseContext : DbContext
+{
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(DatabaseContext).Assembly);
+    }
+}
