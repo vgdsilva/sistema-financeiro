f834001268acc595f39d026ab8146222daa7358d
vinicius.silva <gasparellovinicius@gmail.com>
2026-05-25 12:03:27 -0300

    feat: remover codigo não utilizavel agora

--- src/shared/SistemaFinanceiro.Dominio/Enums/AuditOperacaoEnum.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SistemaFinanceiro.Dominio.Enums;
-
-public enum AuditOperacaoEnum
-{
-    CREATE = 0,
-    UPDATE = 1,
-    DELETE = 2
-}
--- src/shared/SistemaFinanceiro.Dominio/Enums/TipoTransacao.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SistemaFinanceiro.Dominio.Enums;
-
-public enum TipoTransacao
-{
-    Entrada = 0,
-    Saida = 1
-}
