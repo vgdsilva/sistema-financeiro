28719dc1094205e676abf10d35113b34595a9912
vinicius.silva <gasparellovinicius@gmail.com>
2026-05-25 12:05:25 -0300

    feat: criado endpoint de usuario e adicionado referencia ao projeto Infra.Data

---
+++ src/server/SistemaFinanceiro.Server/Controllers/UsuarioController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SistemaFinanceiro.Dominio.Entidades;
+
+namespace SistemaFinanceiro.Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
+public class UsuarioController : ControllerBase
+{
+    private readonly ILogger<UsuarioController> _logger;
+
+    public UsuarioController(ILogger<UsuarioController> logger)
+    {
+        _logger = logger;
+    }
+
+
+    //[HttpGet]
+    //[AllowAnonymous]
+    //[ProducesResponseType(typeof(PagedResult<UsuarioDto>), StatusCodes.Status200OK)]
+    //public async Task<ActionResult<PagedResult<BookDto>>> GetUsuarios([FromQuery] BookSearchCriteria criteria, CancellationToken cancellationToken = default)
+    //{
+    //    return Ok(result);
+    //}
+
+    [HttpGet("{id:int}")]
+    [ProducesResponseType(typeof(Usuario), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<Usuario>> GetUsuario(int id, CancellationToken cancellationToken = default)
+    {
+        return Ok();
+    }
+
+    [HttpPost("{id:Guid}")]
+    [Authorize(Roles = "User,Administrator")]
+    [ProducesResponseType(typeof(Usuario), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<Usuario>> SaveUsuario([FromBody] Usuario usuario, CancellationToken cancellationToken = default)
+    {
+        return Ok(usuario);
+    }
+
+    [HttpDelete("{id:int}")]
+    [Authorize(Roles = "Administrator")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> DeleteUsuario(Guid id, CancellationToken cancellationToken = default)
+    {
+        return NoContent();
+    }
+}
--- src/server/SistemaFinanceiro.Server/Program.cs
+++ src/server/SistemaFinanceiro.Server/Program.cs
@@ -1,4 +1,8 @@
 
+using Microsoft.EntityFrameworkCore;
+using SistemaFinanceiro.Server.Infra.Data;
+using SistemaFinanceiro.Server.Infra.Data.Repositories;
+
 namespace SistemaFinanceiro.Server;
 
 public class Program
@@ -8,10 +12,26 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
-
         builder.Services.AddControllers();
+
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
         builder.Services.AddOpenApi();
+
+        var connectionsString = builder.Configuration.GetConnectionString("PostgreSQL");
+
+        builder.Services.AddDbContext<SistemaFinanceiroContext>(options =>
+        {
+            options.UseNpgsql(connectionsString, sqlOptions =>
+            {
+                sqlOptions.CommandTimeout(30);
+                sqlOptions.EnableRetryOnFailure(maxRetryCount: 3, maxRetryDelay: TimeSpan.FromSeconds(5), errorCodesToAdd: null);
+            });
+
+        }, ServiceLifetime.Scoped);
+
+        builder.Services.AddDbContextPool<SistemaFinanceiroContext>(options => { options.UseNpgsql(connectionsString); }, poolSize: 120);
+
+        builder.Services.AddScoped<UsuarioRepository>();
 
         var app = builder.Build();
 
